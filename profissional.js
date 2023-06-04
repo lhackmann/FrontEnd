63c85eed9f7bf5bbbd335df06f9e6caadbebfac5
Leonardo Cardoso <leonardohcardoso15@gmail.com>
2023-06-04 15:24:02 -0300

    correção profissional

--- profissional.js
+++ profissional.js
@@ -1,3 +1,167 @@
+function atualizarProfissionais(){
+   const ul= document.getElementById('listaProfissionais')
+    ul.innerHTML = ''
+
+    const profissionais = fetch('http://localhost:3000/profissionais')
+    .then(resposta => resposta.json())
+    .then(profissionaisDados => {
+        profissionaisDados.forEach(profissional => {
+            const li = document.createElement('li')
+            li.textContent = `${profissional.idProfissional} - ${profissional.nome} - ${profissional.cpf} - ${profissional.rg} - ${profissional.profissao} - ${profissional.ccr} - ${profissional.assinatura}`
+            
+            const btnDelete = document.createElement('button')
+            btnDelete.innerText = "Remover"
+            btnDelete.addEventListener('click', () => deletarProfissional(profissional.idProfissional))
+            li.appendChild(btnDelete)
+
+          const botaoAtualizar = document.createElement('button')
+            botaoAtualizar.textContent = 'Atualizar'
+            botaoAtualizar.className = 'btn btn-warning m-1'
+            botaoAtualizar.addEventListener('click', () => showProfissional(profissional))
+
+            const ul = document.getElementById('listaProfissionais')
+            ul.appendChild(li)
+        })
+})
+}
+
+function showProfissional(profissional) {
+    document.getElementById('idProfissionalUpdate').value = profissional.idProfissional
+    document.getElementById('nomeUpdate').value = profissional.nome
+    document.getElementById('cpfUpdate').value = profissional.cpf
+    document.getElementById('rgUpdate').value = profissional.rg
+    document.getElementById('profissaoUpdate').value = profissional.profissao
+    document.getElementById('ccrUpdate').value = profissional.ccr
+    document.getElementById('assinaturaUpdate').value = profissional.assinatura
+}
+
+function deletarProfissional(id) {
+    fetch(`http://localhost:3000/profissionais/${id}`, {
+        method: 'DELETE'
+    }).then(resposta => {
+        if(resposta.status !=200){
+            alert('Erro ao exluir profissional')
+        }
+        alert('Profissional excluído com sucesso')
+       
+    })
+}
+
+atualizarProfissionais()
+
+document.getElementById("formCadastro").addEventListener("submit", function (event) {
+    event.preventDefault()
+    cadastrarProfissional(event)
+});
+
+function cadastrarProfissional(form) {
+    const Profissional = {
+        idProfissional: form.target.idProfissional.value,
+        nome: form.target.nome.value,
+        CPF: form.target.cpf.value,
+        RG: form.target.rg.value,
+        profissao: form.target.profissao.value,
+        CCR: form.target.ccr.value,
+        assinatura: form.target.assinatura.value
+    }
+
+    fetch('http://localhost:3000/profissionais', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(Profissional)
+    }).then(resposta => {
+        if (resposta.status !=200 && resposta.status != 201) {
+            alert('Erro ao cadastrar profissional!')
+        }
+        alert('Profissional cadastrado com sucesso!')
+        form.target.reset()
+        atualizarProfissionais()
+    })
+
+}
+
+document.getElementById("formUpdate").addEventListener("submit", function (event) {
+    event.preventDefault()
+    atualizarProfissionais(event)
+});
+
+function atualizarProfissionais(form) {
+    const profissional = {
+        idProfissional: form.target.idProfissional.value,
+        nome: form.target.nomeUpdate.value,
+        CPF: form.target.cpfUpdate.value,
+        RG: form.target.rgUpdate.value,
+        profissao: form.target.profissaoUpdate.value,
+        CCR: form.target.ccrUpdate.value,
+        assinatura: form.target.assinaturaUpdate.value
+    }
+
+    fetch(`http://localhost:3000/profissionais/${form.target.idUpdate.value}`, {
+        method: 'PUT',
+        headers: { 'Content-Type': 'application/json'},
+        body: JSON.stringify(profissional)
+    }).then(resposta => {
+        if (resposta.status !=200) {
+            alert('Erro ao atualizar profisisonal!')
+        }
+        alert('Profissional atualizado com sucesso!')
+        form.target.reset()
+        atualizarProfissionais()
+        document.getElementById('btnUpdate').disabled = true
+    })
+}
+
+
+/*const formCadastro = document.getElementById('formCadastro')
+formCadastro.addEventListener('submit',(event) => {
+    event.preventDefault()
+    cadastrarProfissional(event)
+})
+
+function cadastrarProfissional(form){
+    const profissionalNovo = {
+        idProfissional: form.target.idProfissional.value,
+        nome: form.target.nome.value,
+        cpf: form.target.cpf.value,
+        rg: form.target.rg.value,
+        profissao: form.target.profissao.value,
+        ccr: form.target.ccr.value,
+        assinatura: form.target.assinatura.value
+    }
+    fetch('http://localhost:3000/profissionais',{
+        method: 'POST',
+        headers: { 'Content-type' : 'application/json'},
+        body: JSON.stringify(profissionalNovo)
+    })
+    .then(resposta => {
+        if(resposta.status != 201){
+            alert('Erro ao cadastrar! Por favor inserir todos os dados válidos!')
+            return
+        }
+
+        alert('Cadastrado com sucesso!')
+        atualizarProfissionais()
+    })
+}
+
+function deletarProfissional(id){
+    fetch(`http://localhost:3000/profissionais/${id}`,{
+        method: 'DELETE'
+    })
+    .then(resposta => {
+        if(resposta.status != 200){
+            alert('Erro ao excluir!')
+        }
+        atualizarProfissionais()
+    })
+}//lucas */
+
+
+
+
+
+
+
 /*function atualizarProfissionais() {
     const ul = document.getElementById('listaProfissionais')
     ul.innerHTML = ''
@@ -76,111 +240,3 @@ document.body.appendChild(outroP)
 
 const titulo = document.getElementById('titulo')
 titulo.innerText = "H1 alterado pelo JS"*/
-
-function atualizarProfissional(){
-    document.getElementById('listaProfissional').innerHTML = ''
-    const profissional = fetch('http://localhost:3000/profissional')
-    .then(resposta => resposta.json())
-    .then(profissional => {
-
-        profissional.forEach(profissional => {
-            const li = document.createElement('li')
-            li.textContent = `${profissional.nome} - ${profissional.cpf} - ${profissional.rg} - ${profissional.profissao} - ${profissional.ccr} - ${profissional.assinatura}`
-            const botaoExcluir = document.createElement('button')
-            botaoExcluir.textContent = 'Excluir'
-            botaoExcluir.className = 'btn btn-danger m-1'
-            botaoExcluir.addEventListener('click', () => deleteProfissional(profissional.id))
-            li.appendChild(botaoExcluir)
-
-            const botaoAtualizar = document.createElement('button')
-            botaoAtualizar.textContent = 'Atualizar'
-            botaoAtualizar.className = 'btn btn-warning m-1'
-            botaoAtualizar.addEventListener('click', () => showProfissional(profissional))
-
-            document.getElementById('listaProfissional').appendChild(li)
-        })
-    })
-}
-
-function showProfissional(profissional) {
-    document.getElementById('nomeUpdate').value = profissional.nome
-    document.getElementById('cpfUpdate').value = profissional.cpf
-    document.getElementById('rgUpdate').value = profissional.rg
-    document.getElementById('profissaoUpdate').value = profissional.profissao
-    document.getElementById('ccrUpdate').value = profissional.ccr
-    document.getElementById('assinaturaUpdate').value = profissional.assinatura
-}
-
-function deleteProfissional(id) {
-    fetch(`http://localhost:3000/profissional/${id}`, {
-        method: 'DELETE'
-    }).then(resposta => {
-        if(resposta.status !=200){
-            alert('Erro ao exluir profissional')
-        }
-        alert('Profissional excluído com sucesso')
-        atualizarProfissional()
-    })
-}
-
-atualizarProfissional()
-
-document.getElementById("formCadastro").addEventListener("submit", function (event) {
-    event.preventDefault()
-    cadastrarProfissional(event)
-});
-
-function cadastrarProfissional(form) {
-    const Profissional = {
-        nome: form.target.nome.value,
-        CPF: form.target.cpf.value,
-        RG: form.target.rg.value,
-        profissao: form.target.profissao.value,
-        CCR: form.target.ccr.value,
-        assinatura: form.target.assinatura.value
-    }
-
-    fetch('http://localhost:3000/profissional', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify(profissional)
-    }).then(resposta => {
-        if (resposta.status !=200 && resposta.status != 201) {
-            alert('Erro ao cadastrar profissional!')
-        }
-        alert('Profissional cadastrado com sucesso!')
-        form.target.reset()
-        atualizarProfissional()
-    })
-
-}
-
-document.getElementById("formUpdate").addEventListener("submit", function (event) {
-    event.preventDefault()
-    atualizarProfissional(event)
-});
-
-function atualizarProfissional(form) {
-    const profissional = {
-        nome: form.target.nomeUpdate.value,
-        CPF: form.target.cpfUpdate.value,
-        RG: form.target.rgUpdate.value,
-        profissao: form.target.profissaoUpdate.value,
-        CCR: form.target.ccrUpdate.value,
-        assinatura: form.target.assinaturaUpdate.value
-    }
-
-    fetch(`http://localhost:3000/profissional/${form.target.idUpdate.value}`, {
-        method: 'PUT',
-        headers: { 'Content-Type': 'application/json'},
-        body: JSON.stringify(profissional)
-    }).then(reposta => {
-        if (resposta.status !=200) {
-            alert('Erro ao atualizar profisisonal!')
-        }
-        alert('Profissional atualizado com sucesso!')
-        form.target.reset()
-        atualizarProfissional()
-        document.getElementById('btnUpdate').disabled = true
-    })
-}
